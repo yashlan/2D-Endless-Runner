414fe79ecaf3576e8c31191484c3e667ab29bf01
yashlan <yashlan007@gmail.com>
2021-09-13 19:36:40 +0700

    done

---
+++ Assets/scripts/CharacterSoundController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip scoreHighlight;
+    [SerializeField]
+    private AudioClip jumpSound;
+    [SerializeField]
+    private AudioSource audioSource;
+
+    public AudioClip ScoreHighLight => scoreHighlight;
+    public AudioClip JumpSound => jumpSound;
+
+    public void PlaySFX(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
